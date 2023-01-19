78a2d85140a75cfadd0e33eea8c773da6cec3bc1
Shrilakshmi shenoy <69861469+shrilakshmi-24@users.noreply.github.com>
2023-01-19 11:45:20 +0530

    Add files via upload

---
+++ hoisting.js
@@ -0,0 +1,18 @@
+console.log(a)//udefined
+
+var a=5
+
+// console.log(k) //throws error
+// let k=10
+
+demo()
+
+function demo(){
+    console.log("Its working")
+}
+
+
+hello();//throws error
+hello=()=>{
+    console.log("hello")
+}
